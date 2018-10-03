cade1db63fb2f3e5003117fee8f4129a77ecf72f
hamza tariq <hamza-tariq@hotmail.com>
2018-10-03 11:05:43 +0500

    save

---
+++ PoSCloudApp/Core/Models/DbModels/ExpenseHead.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PoSCloudApp.Core.Models.DbModels
+{
+    public class ExpenseHead:AuditableEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
