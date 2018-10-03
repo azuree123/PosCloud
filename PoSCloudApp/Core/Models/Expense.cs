54f728619a62a588f2352dab5ef75a0c396f8bac
hamza tariq <hamza-tariq@hotmail.com>
2018-10-03 10:57:09 +0500

    save

---
+++ PoSCloudApp/Core/Models/Expense.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PoSCloudApp.Core.Models.DbModels;
+
+namespace PoSCloudApp.Core.Models
+{
+    public class Expense:AuditableEntity
+    {
+        
+    }
+}
