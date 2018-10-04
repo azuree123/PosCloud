ab238d0e8c9b19336b58494abd5c8bfc097fbd8c
hamza tariq <hamza-tariq@hotmail.com>
2018-10-04 12:40:19 +0500

    Go

--- PoSCloudApp/Controllers/ProductsController.cs
+++ PoSCloudApp/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PoSCloudApp.Core;
+using PoSCloudApp.Core.Models;
 using PoSCloudApp.Persistence;
 
 namespace PoSCloudApp.Controllers
@@ -28,7 +29,52 @@ namespace PoSCloudApp.Controllers
 
         public ActionResult AddProduct()
         {
+            ViewBag.edit = "AddProduct";
             return View();
+        }
+        [HttpPost]
+        public ActionResult AddProduct(Product product)
+        {
+            ViewBag.edit = "AddProduct";
+            if (ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                _unitOfWork.ProductRepository.AddProduct(product);
+                _unitOfWork.Complete();
+                return RedirectToAction("ProductsList", "Products");
+            }
+            
+        }
+        public ActionResult UpdateProduct(int id)
+        {
+            ViewBag.edit = "UpdateProduct";
+            Product product = _unitOfWork.ProductRepository.GetProductById(id);
+            return View("AddProduct", product);
+        }
+        [HttpPost]
+        public ActionResult UpdateProduct(int id,Product product)
+        {
+            ViewBag.edit = "UpdateProduct";
+            if (ModelState.IsValid)
+            {
+                return View("AddProduct", product);
+            }
+            else
+            {
+                _unitOfWork.ProductRepository.UpdateProduct(id,product);
+                _unitOfWork.Complete();
+                return RedirectToAction("ProductsList", "Products");
+            }
+
+        }
+        public ActionResult DeleteProduct(int id)
+        {
+            _unitOfWork.ProductRepository.DeleteProduct(id);
+            _unitOfWork.Complete();
+            return RedirectToAction("ProductsList", "Products");
         }
     }
 }
