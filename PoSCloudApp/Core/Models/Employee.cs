47acc942880b0364bc91993af705a044e2ddbb75
m.waqar <m.waqar@F-HQ-PC-8>
2018-10-04 12:40:43 +0500

    go

--- PoSCloudApp/Core/Models/Employee.cs
+++ PoSCloudApp/Core/Models/Employee.cs
@@ -10,6 +10,7 @@ namespace PoSCloudApp.Core.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public string Gender { get; set; }
         public string MobileNumber { get; set; }
         public double Salary { get; set; }
         public double Commission { get; set; }
---
+++ PoSCloudApp/Migrations/201810040713549_Update-3.cs
@@ -0,0 +1,18 @@
+namespace PoSCloudApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Update3 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Employees", "Gender", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Employees", "Gender");
+        }
+    }
+}
