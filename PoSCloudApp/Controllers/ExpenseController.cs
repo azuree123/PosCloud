b07da7049eee8a3ea645b0cce478f70d8cad9583
hamza tariq <hamza-tariq@hotmail.com>
2018-10-05 12:16:18 +0500

    Expense Head created

--- PoSCloudApp/Controllers/ExpenseController.cs
+++ PoSCloudApp/Controllers/ExpenseController.cs
@@ -102,11 +102,47 @@ namespace PoSCloudApp.Controllers
         }
         public ActionResult AddExpenseHead()
         {
+            ViewBag.edit = "AddExpenseHead";
             return View();
         }
-        public ActionResult UpdateExpenseHead()
+        [HttpPost]
+        public ActionResult AddExpenseHead(ExpenseHeadViewModel expenseHeadVm)
         {
-            return View();
+            ViewBag.edit = "AddExpenseHead";
+            if (!ModelState.IsValid)
+            {
+                return View(expenseHeadVm);
+            }
+            else
+            {
+                ExpenseHead expenseHead = Mapper.Map<ExpenseHead>(expenseHeadVm);
+                _unitOfWork.ExpenseHeadRepository.AddExpenseHead(expenseHead);
+                _unitOfWork.Complete();
+                return RedirectToAction("ExpenseHeadList");
+            }
+        }
+        public ActionResult UpdateExpenseHead(int id)
+        {
+            ViewBag.edit = "UpdateExpenseHead";
+            ExpenseHeadViewModel expenseHeadVm =
+                Mapper.Map<ExpenseHeadViewModel>(_unitOfWork.ExpenseHeadRepository.GetExpenseHeadById(id));
+            return View("AddExpenseHead", expenseHeadVm);
+        }
+        [HttpPost]
+        public ActionResult UpdateExpenseHead(int id,ExpenseHeadViewModel expenseHeadVm)
+        {
+            ViewBag.edit = "UpdateExpenseHead";
+            if (!ModelState.IsValid)
+            {
+                return View("AddExpenseHead", expenseHeadVm);
+            }
+            else
+            {
+                ExpenseHead expenseHead = Mapper.Map<ExpenseHead>(expenseHeadVm);
+                _unitOfWork.ExpenseHeadRepository.UpdateExpenseHead(id,expenseHead);
+                _unitOfWork.Complete();
+                return RedirectToAction("ExpenseHeadList");
+            }
         }
         public ActionResult DeleteExpenseHead(int id)
         {
