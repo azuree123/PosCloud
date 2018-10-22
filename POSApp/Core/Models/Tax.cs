86a049b617a217f6324f961ecc34d01367d1a98b
hamza tariq <hamza-tariq@hotmail.com>
2018-10-22 09:52:40 +0500

    asda

---
+++ POSApp/Core/Models/Tax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace POSApp.Core.Models
+{
+    public class Tax
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DefaultValue(0)]
+        public double Rate { get; set; }
+        public int StoreId { get; set; }
+        public Store Store { get; set; }
+    }
+}
