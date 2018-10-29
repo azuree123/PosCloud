42242c9870eb3243fac41f40eae066f7a0fa86eb
m.waqar <m.waqar@F-HQ-PC-8>
2018-10-29 18:33:41 +0500

    save

--- POSApp/Controllers/ExcelImportController.cs
+++ POSApp/Controllers/ExcelImportController.cs
@@ -30,13 +30,9 @@ namespace POSApp.Controllers
         }
 
         // GET: ExcelImport
-        public ActionResult ExcelImport(string action)
+        public ActionResult StateExcelImport()
         {
-            ViewBag.edit = action;
-            if (action.Contains("State"))
-            {
-                ViewBag.download = "/Content/ImportFormats/StateImport.xls";
-            }
+            ViewBag.edit = "StateExcelImport";
             return View();
         }
 
@@ -107,6 +103,81 @@ namespace POSApp.Controllers
 
             }
                 return RedirectToAction("StateList", "Setup");
+        }
+        [HttpGet]
+        public ActionResult CityExcelImport()
+        {
+            ViewBag.edit = "CityExcelImport";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CityExcelImport(HttpPostedFileBase file)
+        {
+            string filepath = string.Empty;
+            if (file != null)
+            {
+                string path = Server.MapPath("~/Content/Uploads/");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                filepath = path + Path.GetFileName(file.FileName);
+                string extension = Path.GetExtension(file.FileName);
+                file.SaveAs(filepath);
+
+                string conString = string.Empty;
+                switch (extension)
+                {
+                    case ".xls":
+                        conString = ConfigurationManager.ConnectionStrings["Excel03ConString"].ConnectionString;
+                        break;
+                    case ".xlsx":
+                        conString = ConfigurationManager.ConnectionStrings["Excel07ConString"].ConnectionString;
+                        break;
+                }
+
+                DataTable dt = new DataTable();
+                conString = string.Format(conString, filepath);
+
+                using (OleDbConnection conExcel = new OleDbConnection(conString))
+                {
+                    using (OleDbCommand cmdExcel = new OleDbCommand())
+                    {
+                        using (OleDbDataAdapter odaExcel = new OleDbDataAdapter())
+                        {
+                            cmdExcel.Connection = conExcel;
+                            conExcel.Open();
+                            DataTable dtExcelSchema;
+                            dtExcelSchema = conExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                            string sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+                            conExcel.Close();
+                            conExcel.Open();
+                            cmdExcel.CommandText = "SELECT * From [" + sheetName + "]";
+                            odaExcel.SelectCommand = cmdExcel;
+                            odaExcel.Fill(dt);
+                            conExcel.Close();
+                        }
+
+                    }
+                }
+
+                //var userid = User.Identity.GetUserId();
+                //var user = UserManager.FindById(userid);
+                foreach (DataRow dr in dt.Rows)
+                {
+                    City NewModel = new City();
+                    if (!string.IsNullOrWhiteSpace(dr["Name"].ToString()))
+                    {
+                        NewModel.Name = dr["Name"].ToString();
+                        NewModel.StateId = Int32.Parse(dr["StateId"].ToString());
+                        _unitOfWork.CityRepository.AddCity(NewModel);
+                    }
+                }
+                _unitOfWork.Complete();
+
+            }
+            return RedirectToAction("CityList", "Setup");
         }
 
         public ApplicationUserManager UserManager
