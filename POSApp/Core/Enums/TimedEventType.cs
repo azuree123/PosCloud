add959862506f1d9497dce64643c3c371113906a
m.waqar <m.waqar@F-HQ-PC-8>
2018-11-03 17:26:11 +0500

    klj

---
+++ POSApp/Core/Enums/TimedEventType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace POSApp.Core.Enums
+{
+    public enum TimedEventType
+    {
+        FixedPrice,
+        ReducePriceByAmount,
+        ReducePriceByPercentage,
+        IncreasePriceByAmount,
+        IncreasePriceByPercentage
+    }
+}
