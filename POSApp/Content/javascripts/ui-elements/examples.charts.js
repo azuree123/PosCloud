359ba52732b8c25402e4570c9d76438706c040f6
azhar hussain <azhar@123>
2018-11-03 14:39:43 +0500

    aa

--- POSApp/Content/javascripts/ui-elements/examples.charts.js
+++ POSApp/Content/javascripts/ui-elements/examples.charts.js
@@ -52,22 +52,22 @@ Theme Version: 	1.5.2
 	/*
 	Chartist: Bar Chart - Horizontal Chart
 	*/
-    (function () {
-        new Chartist.Bar('#ChartistHorizontalChart', {
-            labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-            series: [
-                [5, 4, 3, 7, 5, 10, 3],
-                [3, 2, 9, 5, 4, 6, 4]
-            ]
-        }, {
-                seriesBarDistance: 10,
-                reverseData: true,
-                horizontalBars: true,
-                axisY: {
-                    offset: 70
-                }
-            });
-    })();
+    //(function () {
+    //    new Chartist.Bar('#ChartistHorizontalChart', {
+    //        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    //        series: [
+    //            [5, 4, 3, 7, 5, 10, 3],
+    //            [3, 2, 9, 5, 4, 6, 4]
+    //        ]
+    //    }, {
+    //            seriesBarDistance: 10,
+    //            reverseData: true,
+    //            horizontalBars: true,
+    //            axisY: {
+    //                offset: 70
+    //            }
+    //        });
+    //})();
 
 	/*
 	Morris: Area
