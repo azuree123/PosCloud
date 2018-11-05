d747c9f2f96d829e675d4135ffeba9a710937679
m.waqar <m.waqar@F-HQ-PC-8>
2018-11-05 17:00:37 +0500

    check

---
+++ POSApp/Core/Models/DineTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace POSApp.Core.Models
+{
+    public class DineTable:AuditableEntity
+    {
+        public int Id { get; set; }
+        public int DineTableNumber { get; set; }
+        public int floorId { get; set; }
+        public virtual Floor Floor { get; set; }
+    }
+}
---
+++ POSApp/Core/Models/Floor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace POSApp.Core.Models
+{
+    public class Floor
+    {
+        public int Id { get; set; }
+        public int FloorNumber { get; set; }
+        public ICollection<DineTable> Tables { get; set; }
+    }
+}
