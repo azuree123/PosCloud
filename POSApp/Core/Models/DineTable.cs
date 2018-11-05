54e3c64f5c0d54afdf57a631f9d13412e621e13c
hamza tariq <hamza-tariq@hotmail.com>
2018-11-05 17:11:25 +0500

    asda

--- POSApp/Core/Models/DineTable.cs
+++ POSApp/Core/Models/DineTable.cs
@@ -8,8 +8,9 @@ namespace POSApp.Core.Models
     public class DineTable:AuditableEntity
     {
         public int Id { get; set; }
-        public int DineTableNumber { get; set; }
-        public int floorId { get; set; }
+        public string DineTableNumber { get; set; }
+        public int FloorId { get; set; }
+        public bool IsBooked { get; set; }
         public virtual Floor Floor { get; set; }
     }
 }
--- POSApp/Core/Models/Floor.cs
+++ POSApp/Core/Models/Floor.cs
@@ -9,7 +9,7 @@ namespace POSApp.Core.Models
     public class Floor
     {
         public int Id { get; set; }
-        public int FloorNumber { get; set; }
+        public string FloorNumber { get; set; }
         public ICollection<DineTable> Tables { get; set; }
     }
 }
