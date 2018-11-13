88d9e2c28d6541027cadfd531351a9df77305488
m.waqar <m.waqar@F-HQ-PC-8>
2018-11-13 11:36:12 +0500

    Fixes

--- POSApp/Core/ViewModels/SecurityObjectViewModel.cs
+++ POSApp/Core/ViewModels/SecurityObjectViewModel.cs
@@ -7,7 +7,7 @@ namespace POSApp.Core.ViewModels
 {
     public class SecurityObjectViewModel
     {
-        public int SecurityObjectId { get; set; }
+        public int? SecurityObjectId { get; set; }
         public string Name { get; set; }// varchar 150
         public string Type { get; set; }// Form, report etc // varchar 15
         public string Module { get; set; }//HR, Finane etc 50
