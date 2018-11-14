d4ef2a6ba596c6d311a6814859069c2038861f37
hamza <hamza@F-HQ-PC5>
2018-11-14 21:14:19 +0500

    Changes

---
+++ POSApp/Core/Enums/Security.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace POSApp.Core.Enums
+{
+    public enum Security
+    {
+        Dashboard=1,
+        SaleOrders=2,
+        PurchaseOrders=3
+        ,Expenses=4,
+        Branches=5,
+        Products=6,
+        Devices=7,
+        Setup=8,
+        UserManagement=9,
+        Reports=10,
+        BusinessSettings=10
+    }
+    
+
+}
