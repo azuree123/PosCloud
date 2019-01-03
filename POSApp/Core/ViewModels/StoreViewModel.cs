2381aebffe9b626949f6596348ee31d4f5d535cc
m.waqar <m.waqar@F-HQ-PC-8>
2019-01-03 12:23:09 +0500

    fixies

--- POSApp/Core/ViewModels/StoreViewModel.cs
+++ POSApp/Core/ViewModels/StoreViewModel.cs
@@ -11,7 +11,7 @@ namespace POSApp.Core.ViewModels
         public int? Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
-        [RegularExpression(@"^\(?([0-9]{4})\)?[-. ]?([0-9]{7})$", ErrorMessage = "Entered phone format like (03211234567)")]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{7})$", ErrorMessage = "Entered phone format like (050xxxxxxx)")]
         public string Contact { get; set; }
 
         public string City { get; set; }
