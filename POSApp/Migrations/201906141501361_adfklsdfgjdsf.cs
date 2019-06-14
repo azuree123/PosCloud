d7ae72b9e8f215e84c5d661f5150925e300498cc
cosmic6988@outlook.com <cosmic6988@outlook.com>
2019-06-14 17:04:08 +0200

    fdsa

---
+++ POSApp/Migrations/201906141501361_adfklsdfgjdsf.cs
@@ -0,0 +1,19 @@
+namespace POSApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class adfklsdfgjdsf : DbMigration
+    {
+        public override void Up()
+        {
+           
+           
+        }
+        
+        public override void Down()
+        {
+           
+        }
+    }
+}
