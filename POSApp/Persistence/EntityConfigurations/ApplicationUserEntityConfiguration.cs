15b28e90037973d8f52b7cf0155ca514cd27efea
m.waqar <m.waqar@F-HQ-PC-8>
2019-06-14 17:36:48 +0500

    qwqw

--- POSApp/Persistence/EntityConfigurations/ApplicationUserEntityConfiguration.cs
+++ POSApp/Persistence/EntityConfigurations/ApplicationUserEntityConfiguration.cs
@@ -14,7 +14,7 @@ namespace POSApp.Persistence.EntityConfigurations
            
             HasRequired(x => x.Store).WithMany().HasForeignKey(x => new { x.StoreId }).WillCascadeOnDelete(false);
             HasOptional(x => x.POSTerminal).WithMany(a=>a.ApplicationUsers).HasForeignKey(x => new {x.POSTerminalId ,x.StoreId }).WillCascadeOnDelete(false);
-            HasOptional(x => x.Employee).WithMany(a => a.ApplicationUsers).HasForeignKey(x => new { x.EmployeeId, x.StoreId }).WillCascadeOnDelete(false);
+            HasRequired(x => x.Employee).WithMany(a => a.ApplicationUsers).HasForeignKey(x => new { x.EmployeeId, x.StoreId }).WillCascadeOnDelete(false);
             
         }
     }
